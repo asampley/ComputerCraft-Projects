85d4f40356d31a3a961c8f55c0b7b02cf5844c4b
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2022-12-28 22:51:11 -0700

    revert bucket as per comment (we want it to be 75% user-error proof :p)

--- lib/bucket.lua
+++ lib/bucket.lua
@@ -6,17 +6,24 @@ local bucketSlot = nil
 -- list of fuels to bucket up
 local fuel = require("/lib/config").load("bucket")
 
--- Assumes the bucket does not change slot within a program's running
 m.find = function()
-  if bucketSlot == nil then
-    for i = 1, 16 do
-      item = turtle.getItemDetail(i)
+  if bucket ~= nil and bucketSlot ~= 0 then
+    local item = turtle.getItemDetail(bucketSlot)
 
-      if item and item.name == "minecraft:bucket" then
-        bucketSlot = i
+    if item and item.name == "minecraft:bucket" then
+      return bucketSlot
+    end
+  end
 
-        break
-      end
+  bucketSlot = 0
+
+  for i = 1, 16 do
+    item = turtle.getItemDetail(i)
+
+    if item and item.name == "minecraft:bucket" then
+      bucketSlot = i
+
+      break
     end
   end
 
