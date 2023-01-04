7736f432b85296a9dab9fb28df7464ce4252a9a9
Alex Sampley <alex.sampley@gmail.com>
2023-01-03 20:53:02 -0700

    Added more options and messaging for positional arguments

--- lib/args.lua
+++ lib/args.lua
@@ -5,6 +5,8 @@ m.parse = function(definition, arguments)
 
   local args = {}
 
+  local position = 1
+
   local iter, invariant, k, v = ipairs(arguments)
 
   while true do
@@ -63,13 +65,94 @@ m.parse = function(definition, arguments)
           error("Unknown type of flag \"" .. flagDef .. "\"")
         end
       else
-        -- append a positional argument
-        args[#args+1] = v
+        local rest = false
+
+        local posDef = definition.required and definition.required[position]
+
+        if not posDef then
+          local offset = (definition.required and #definition.required) or 0
+          posDef = definition.optional and definition.optional[position - offset]
+        end
+
+        if not posDef then
+          posDef = definition.rest
+          rest = true
+        end
+
+        if not posDef then
+          error("Too many arguments supplied")
+        else
+          if not posDef.name then
+            error("'name' not set for positional argument")
+          end
+
+          local parsed
+          if not posDef.type or posDef.type == "string" then
+            parsed = v
+          elseif posDef.type == "number" then
+            parsed = tonumber(v) or error("Expected number for \"" .. posDef.name .. "\"")
+          else
+            error("Unknown type for positional \"" .. posDef.type .. "\"")
+          end
+
+          if rest then
+            if not args[posDef.name] then
+              args[posDef.name] = {}
+            end
+
+            args[posDef.name][#args[posDef.name] + 1] = parsed
+          else
+            args[posDef.name] = parsed
+            position = position + 1
+          end
+        end
       end
     end
   end
 
+  if definition.required and position <= #definition.required then
+    error("Too few arguments supplied")
+  end
+
   return args
+end
+
+m.usage = function(definition)
+  local usage = ""
+
+  if definition.required then
+    for _, def in ipairs(definition.required) do
+      usage = usage .. " " .. def.name
+    end
+  end
+
+  if definition.optional then
+    for _, def in ipairs(definition.optional) do
+      usage = usage .. " [" .. def.name .. "]"
+    end
+  end
+
+  if definition.rest then
+    usage = usage .. " [" .. definition.rest.name .. "...]"
+  end
+
+  for flag, def in pairs(definition.flags) do
+    if #flag > 1 then
+      usage = usage .. " [--" .. flag
+    else
+      usage = usage .. " [-" .. flag
+    end
+
+    if def == "string" or def.type == "string" then
+      usage = usage .. " STRING"
+    elseif def == "number" or def.type == "number" then
+      usage = usage .. " NUMBER"
+    end
+
+    usage = usage .. "]"
+  end
+
+  return usage
 end
 
 return m
