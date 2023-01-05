994dc40ec981c340ba9d34cd25b7b1c018699500
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2023-01-05 16:52:18 -0700

    cleave/layerbore - move main fns to respective bin files

--- bin/cleave.lua
+++ bin/cleave.lua
@@ -1,5 +1,9 @@
 local arguments = require("/lib/args")
 local bore = require("/lib/bore")
+local location = require("/lib/location")
+local move = require("/lib/move")
+local path = require("/lib/path")
+local wants = require("/lib/bore/wants")
 
 local args = arguments.parse({
     flags = { wants = "string" },
@@ -11,11 +15,28 @@ local args = arguments.parse({
   },
   {...})
 
-local dimensionVector = bore.dimensionsToVector(args.forward, args.height, args.right)
-
 local startTime = os.clock()
 
-bore.cleave(dimensionVector, args)
+local dimensionVector = bore.dimensionsToVector(args.forward, args.height, args.right)
+local homePos = location.getPos()
+local homeHeading = location.getHeading()
+
+wants.setProfile(args.wants)
+bore.setChest(homePos)
+
+local success, error = pcall(function()
+  path.rectangleEveryThirdLayer(dimensionVector, function (direction, mustDig)
+
+    bore.smartDig(direction, true, homePos, homeHeading)
+
+  end)
+end)
+if error then print(error) end
+
+move.digTo(homePos, "yzx")
+move.turnTo(homeHeading)
+bore.cleanInventory() -- So we don't drop off stuff picked up on move.digTo
+bore.transferToChest()
 
 local timeTaken = (os.clock() - startTime) / 60
 print("Finished in "..timeTaken.." minutes")
--- bin/layerbore.lua
+++ bin/layerbore.lua
@@ -1,5 +1,9 @@
 local arguments = require("/lib/args")
 local bore = require("/lib/bore")
+local location = require("/lib/location")
+local move = require("/lib/move")
+local path = require("/lib/path")
+local wants = require("/lib/bore/wants")
 
 local args = arguments.parse({
   flags = {
@@ -20,9 +24,43 @@ if args.help then
   return
 end
 
+local startTime = os.clock()
+
 local dimensionVector = bore.dimensionsToVector(args.forward, args.height, args.right)
 
-local startTime = os.clock()
-bore.layerBore(dimensionVector, args)
+local homePos = location.getPos()
+local homeHeading = location.getHeading()
+local toPos = homePos + dimensionVector
+local lastPosition = homePos
+
+wants.setProfile(args.wants)
+bore.setChest(homePos)
+
+local success, error = pcall(function()
+  path.rectangleEveryThirdLayer(toPos, function (direction, mustDig)
+    if mustDig and (direction == "Down" or direction == "Up") and foundBedrock then
+      error("Will not do anymore layers because next layer contains bedrock")
+    end
+
+    if bore.smartDig(direction, mustDig, homePos, homeHeading) == "BEDROCK" then
+      if mustDig then error("Ran into bedrock, ending") end
+      -- If bedrock will block our escape in the y direction
+      if dimensionVector.y >= 0 and direction == "Down" or dimensionVector.y < 0 and direction == "Up" then
+        move.digTo(lastPosition) -- retreat 1 space and end
+        error("Ending because we might get trapped by bedrock")
+      end
+
+      foundBedrock = true
+    end
+    lastPosition = location.getPos()
+  end)
+end)
+if error then print(error) end
+
+move.digTo(homePos, "yzx")
+move.turnTo(homeHeading)
+bore.cleanInventory() -- So we don't drop off stuff picked up on move.digTo
+bore.transferToChest()
+
 local timeTaken = (os.clock() - startTime) / 60
 print("Finished in "..timeTaken.." minutes")
