1bc51c69f1c8136bafb40d38a6e79910b7376939
Alex Sampley <alex.sampley@gmail.com>
2023-01-11 11:26:49 -0700

    Update move to attempt backwards movement if possible

--- lib/move.lua
+++ lib/move.lua
@@ -3,56 +3,60 @@ local location = require("/lib/location")
 local m = {}
 
 -- move the turtle to the position
-local function _to(position, upFunc, downFunc, forwardFunc, digUpFunc, digDownFunc, digFunc, orderOfMoves)
+local function _to(position, upFunc, downFunc, forwardFunc, backFunc, digUpFunc, digDownFunc, digFunc, orderOfMoves)
   local moveOrder = orderOfMoves or "xzy"
   local currPosition = location.getPos()
   local path = position - currPosition
 
+  print(position, currPosition, path)
+
   -- move in each axis, xyz
   for i = 1, 3 do
-    local move = moveOrder:sub(i, i)
-    if move == "x"
-    then
-      if path.x < 0
-      then
-        m.turnTo(vector.new(-1, 0, 0))
-        path.x = -(path.x)
-      elseif path.x > 0
-      then
-        m.turnTo(vector.new(1, 0, 0))
-      end
-      for _ = 1, path.x do
-        while not forwardFunc() do
-          if digFunc then digFunc() end
+    local move = moveOrder:sub(i, i):lower()
+
+    if path[move] ~= 0 then
+      if move == "x" or move == "z" then
+        local forward = location[move:upper()]()
+        if path[move] < 0 then
+          forward = -forward
         end
-      end
-    elseif move == "z"
-    then
-      if path.z < 0
-      then
-        m.turnTo(vector.new(0, 0, -1))
-        path.z = -(path.z)
-      elseif path.z > 0
-      then
-        m.turnTo(vector.new(0, 0, 1))
-      end
-      for _ = 1, path.z do
-        while not forwardFunc() do
-          if digFunc then digFunc() end
+
+        local heading = location.getHeading()
+        local back = heading[move] == -forward[move]
+
+        if not heading[move] == forward[move] or not back then
+          m.turnTo(forward)
         end
-      end
-    else
-      if path.y < 0
-      then
-        for _ = 1, -(path.y) do
-          while not downFunc() do
-            if digDownFunc then digDownFunc() end
+
+        while path[move] ~= 0 do
+          if back then
+            if backFunc() then
+              path[move] = path[move] - forward[move]
+            else
+              m.turnTo(forward)
+              back = false
+            end
+          else
+            if forwardFunc() then
+              path[move] = path[move] - forward[move]
+            else
+              digFunc()
+            end
           end
         end
       else
-        for _ = 1, path.y do
-          while not upFunc() do
-            if digUpFunc then digUpFunc() end
+        if path.y < 0
+        then
+          for _ = 1, -(path.y) do
+            while not downFunc() do
+              if digDownFunc then digDownFunc() end
+            end
+          end
+        else
+          for _ = 1, path.y do
+            while not upFunc() do
+              if digUpFunc then digUpFunc() end
+            end
           end
         end
       end
@@ -62,12 +66,12 @@ end
 
 -- go to
 m.goTo = function(position, moveOrder)
-  _to(position, turtle.up, turtle.down, turtle.forward, nil, nil, nil, moveOrder)
+  _to(position, turtle.up, turtle.down, turtle.forward, turtle.back, nil, nil, nil, moveOrder)
 end
 
 -- dig to
 m.digTo = function(position, moveOrder)
-  _to(position, turtle.up, turtle.down, turtle.forward, turtle.digUp, turtle.digDown, turtle.dig, moveOrder)
+  _to(position, turtle.up, turtle.down, turtle.forward, turtle.back, turtle.digUp, turtle.digDown, turtle.dig, moveOrder)
 end
 
 -- turn the turtle to the heading in as few turns as possible
